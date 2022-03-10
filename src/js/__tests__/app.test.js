1fbf5c5a4254d1cf285c5c27b50c91b820a1d9e6
jenyadio <jenyadio@gmail.com>
2022-03-10 23:37:02 +0300

    test added

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,19 @@
+import indicateHealth from '../app';
+
+test('should indicate health', () => {
+    const value = {name: 'Маг', health: 90};
+    const result = indicateHealth(value);
+    expect(result).toBe('healthy');
+});
+
+test('should indicate health', () => {
+    const value = {name: 'Маг', health: 25};
+    const result = indicateHealth(value);
+    expect(result).toBe('wounded');
+});
+
+test('should indicate health', () => {
+    const value = {name: 'Маг', health: 5};
+    const result = indicateHealth(value);
+    expect(result).toBe('critical');
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('app worked');
-
-console.log(sum([1, 2]));
+export default function indicateHealth(indicator) {
+    let result = 0;
+    if (indicator.health > 50) {
+        result = 'healthy';
+    } else if (indicator.health < 50 && indicator.health > 15) {
+        result = 'wounded';
+    } else {
+        result = 'critical';
+    }
+    return result;
+}
