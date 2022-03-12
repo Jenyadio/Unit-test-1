c3b9017535a63fe367a787478863554b1ec9792c
jenyadio <jenyadio@gmail.com>
2022-03-12 14:01:39 +0300

    fixed eslint

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,19 +1,19 @@
 import indicateHealth from '../app';
 
 test('should indicate health', () => {
-    const value = {name: 'Маг', health: 90};
-    const result = indicateHealth(value);
-    expect(result).toBe('healthy');
+  const value = { name: 'Маг', health: 90 };
+  const result = indicateHealth(value);
+  expect(result).toBe('healthy');
 });
 
 test('should indicate health', () => {
-    const value = {name: 'Маг', health: 25};
-    const result = indicateHealth(value);
-    expect(result).toBe('wounded');
+  const value = { name: 'Маг', health: 25 };
+  const result = indicateHealth(value);
+  expect(result).toBe('wounded');
 });
 
 test('should indicate health', () => {
-    const value = {name: 'Маг', health: 5};
-    const result = indicateHealth(value);
-    expect(result).toBe('critical');
+  const value = { name: 'Маг', health: 5 };
+  const result = indicateHealth(value);
+  expect(result).toBe('critical');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 export default function indicateHealth(indicator) {
-    let result = 0;
-    if (indicator.health > 50) {
-        result = 'healthy';
-    } else if (indicator.health < 50 && indicator.health > 15) {
-        result = 'wounded';
-    } else {
-        result = 'critical';
-    }
-    return result;
+  let result = 0;
+  if (indicator.health > 50) {
+    result = 'healthy';
+  } else if (indicator.health < 50 && indicator.health > 15) {
+    result = 'wounded';
+  } else {
+    result = 'critical';
+  }
+  return result;
 }
